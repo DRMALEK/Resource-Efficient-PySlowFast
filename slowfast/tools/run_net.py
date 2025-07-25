6dc4bfc98f4d19c57eb3c5e3d0cab94deed87ce0
milkyway <malek97ai@gmail.com>
2025-07-25 19:04:54 +0200

    commit before staring pipeline branch

--- slowfast/tools/run_net.py
+++ slowfast/tools/run_net.py
@@ -10,7 +10,6 @@ import sys
 sys.path.insert(0, '/home/malek/Master Thesis/Code/slowfast')
 
 
-
 from slowfast.config.defaults import assert_and_infer_cfg
 from slowfast.utils.misc import launch_job
 from slowfast.utils.parser import load_config, parse_args
@@ -18,6 +17,7 @@ from demo_net import demo
 from test_net import test
 from train_net import train
 from visualization import visualize
+
 
 
 def main():
@@ -34,6 +34,14 @@ def main():
         if cfg.TRAIN.ENABLE:
             launch_job(cfg=cfg, init_method=args.init_method, func=train)
 
+        # Perform Pruning (Pruning and benchmarking).
+        if cfg.PRUNING.ENABLE:
+            pass
+
+        # Perform Fine-tuning.
+        if cfg.FINE_TUNING.ENABLE:
+            pass
+
         # Perform multi-clip testing.
         if cfg.TEST.ENABLE:
             if cfg.TEST.NUM_ENSEMBLE_VIEWS == -1:
@@ -44,13 +52,13 @@ def main():
             else:
                 launch_job(cfg=cfg, init_method=args.init_method, func=test)
 
-        # Perform model visualization.
+        # Perform model visualization (optional).
         if cfg.TENSORBOARD.ENABLE and (
             cfg.TENSORBOARD.MODEL_VIS.ENABLE or cfg.TENSORBOARD.WRONG_PRED_VIS.ENABLE
         ):
             launch_job(cfg=cfg, init_method=args.init_method, func=visualize)
 
-        # Run demo.
+        # Run demo (for deployment).
         if cfg.DEMO.ENABLE:
             demo(cfg)
 
